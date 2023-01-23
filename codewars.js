339670ef2fe6a3d6228c7e4b11bd1ae2f2cc0ebe
Arkadiy <72151941+magomedov777@users.noreply.github.com>
2023-01-23 18:37:08 +0200

    Add files via upload

--- codewars.js
+++ codewars.js
@@ -1,3 +1,4 @@
+//First kata
 function evalObject(value){
   switch(value.operation){
     case'+': return value.a + value.b;
@@ -6,14 +7,18 @@ function evalObject(value){
     case'*': return value.a * value.b;
     case'%': return value.a % value.b;
     case'^': return Math.pow(value.a, value.b);
-  }
-}
-//First kata
-
+  }};
+  
+//New kata
 function myFirstKata(a,b) {
 if (typeof(a) === 'number' && typeof(b) === 'number') {
 return (a % b) + (b % a);
 } else {
 return false;
-}
-}
+}};
+
+function warnTheSheep(queue) {
+  const position = queue.reverse().indexOf('wolf');
+
+  return position === 0 ? 'Pls go away and stop eating my sheep' : `Oi! Sheep number ${ position }! You are about to be eaten by a wolf!`;
+};
