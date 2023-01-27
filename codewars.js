6cd400761807c605ddb00409bcd6e99f34d7f257
Arkadiy <72151941+magomedov777@users.noreply.github.com>
2023-01-27 05:46:39 +0200

    Add files via upload

--- codewars.js
+++ codewars.js
@@ -24,3 +24,73 @@ function warnTheSheep(queue) {
   return position === 0 ? 'Pls go away and stop eating my sheep' : `Oi! Sheep number ${ position }! You are about to be eaten by a wolf!`;
 };
 console.log(queue);
+
+
+function zipWith(fn,a0,a1) {
+  let arr = [];
+  const short = Math.min(a0.length, a1.length); 
+  for (let i=0;i<short;i++){
+    arr.push(fn(a0[i],a1[i]))
+  }
+  return arr
+};
+
+function removeFirstTwo(list) {
+  const [, , ...shorterList] = list;
+  return shorterList;
+}
+
+const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const sourceWithoutFirstTwo = removeFirstTwo(source);
+
+const stats = {
+  max: 56.78,
+  standard_deviation: 4.34,
+  median: 34.54,
+  mode: 23.87,
+  min: -0.75,
+  average: 35.85
+};
+const half = ({ max, min }) => (max + min) / 2.0;
+
+function updateRecords(records, id, prop, value) {
+  if (prop !== "tracks" && value !== "") {
+    records[id][prop] = value;
+  } else if (prop === "tracks" && value !== "" && records[id].hasOwnProperty("tracks") === false) {
+    records[id][prop] = [value];
+  } else if (prop === "tracks" && value !== "") {
+    records[id][prop].push(value);
+  } else if (value === "") {
+    delete records[id][prop];
+  }
+  return records;
+}
+
+const myArray = [];
+  for (var i = 1; i < 6; i++) {
+  myArray.push(i);
+};
+
+function updateRecords(records, id, prop, value) {
+  if (prop !== "tracks" && value !== "") {
+    records[id][prop] = value;
+  } else if (prop === "tracks" && value !== "" && records[id].hasOwnProperty("tracks") === false) {
+    records[id][prop] = [value];
+  } else if (prop === "tracks" && value !== "") {
+    records[id][prop].push(value);
+  } else if (value === "") {
+    delete records[id][prop];
+  }
+  return records;
+};
+
+
+function testElseIf(val) {
+  if (val > 10) {
+    return "Greater than 10";
+  } else if (val < 5) {
+    return "Smaller than 5";
+  } else {
+    return "Between 5 and 10";
+  }
+};
