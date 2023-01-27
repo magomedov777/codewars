aa69d8155ec934e31aaa74e6810074eca9135ad0
Arkadiy <72151941+magomedov777@users.noreply.github.com>
2023-01-27 05:48:03 +0200

    Add files via upload

--- codewars.js
+++ codewars.js
@@ -35,24 +35,6 @@ function zipWith(fn,a0,a1) {
   return arr
 };
 
-function removeFirstTwo(list) {
-  const [, , ...shorterList] = list;
-  return shorterList;
-}
-
-const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const sourceWithoutFirstTwo = removeFirstTwo(source);
-
-const stats = {
-  max: 56.78,
-  standard_deviation: 4.34,
-  median: 34.54,
-  mode: 23.87,
-  min: -0.75,
-  average: 35.85
-};
-const half = ({ max, min }) => (max + min) / 2.0;
-
 function updateRecords(records, id, prop, value) {
   if (prop !== "tracks" && value !== "") {
     records[id][prop] = value;
@@ -94,3 +76,22 @@ function testElseIf(val) {
     return "Between 5 and 10";
   }
 };
+
+function removeFirstTwo(list) {
+  const [, , ...shorterList] = list;
+  return shorterList;
+}
+
+const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const sourceWithoutFirstTwo = removeFirstTwo(source);
+
+const stats = {
+  max: 56.78,
+  standard_deviation: 4.34,
+  median: 34.54,
+  mode: 23.87,
+  min: -0.75,
+  average: 35.85
+};
+
+const half = ({ max, min }) => (max + min) / 2.0;
