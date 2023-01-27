2955b6b9be56973d9d2acd28c76330663590d670
Arkadiy <72151941+magomedov777@users.noreply.github.com>
2023-01-27 05:56:19 +0200

    Add files via upload

--- codewars.js
+++ codewars.js
@@ -100,3 +100,15 @@ const half = ({ max, min }) => (max + min) / 2.0;
 function firstNonRepeatingLetter(s) {
   return s[s.toLowerCase().split('').findIndex(letter => s.toLowerCase().split('').filter(l => l === letter).length === 1 )] || '';
 }
+
+function balancedNum(number) {
+  const stringNum = String(number);
+  const numbers = Array.from(
+    { length: (stringNum.length - 1) / 2 },
+    (_, idx) => stringNum[idx] - stringNum[stringNum.length - 1 - idx]
+  );
+
+  return numbers.reduce((total, number) => total + number, 0) === 0
+    ? 'Balanced'
+    : 'Not Balanced';
+}
