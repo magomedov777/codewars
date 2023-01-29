5658e75f367230c20346bca40de7f67874b25940
Arkadiy <72151941+magomedov777@users.noreply.github.com>
2023-01-29 17:11:05 +0200

    Add files via upload

--- codewars.js
+++ codewars.js
@@ -108,8 +108,28 @@ function balancedNum(number) {
   return numbers.reduce((total, number) => total + number, 0) === 0
     ? 'Balanced'
     : 'Not Balanced';
-}
+};
 
+
+
+function whatday(num) {
+  if(num < 1 || num > 7){
+    return "Wrong, please enter a number between 1 and 7"
+  }else if(num === 1){
+   return "Sunday"
+ }else if(num === 2){
+   return "Monday"
+ }else if(num === 3){
+   return "Tuesday"
+ }else if(num === 4){
+   return "Wednesday"
+ }else if(num === 5){
+   return "Thursday"
+ }else if(num === 6){
+   return "Friday"
+ }else if(num === 7){
+   return "Saturday"
+ };
 
 
 
