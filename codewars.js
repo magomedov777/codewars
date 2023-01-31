d38d3f769727160572ee34643e85bcbbcec7e3cc
Arkadiy <72151941+magomedov777@users.noreply.github.com>
2023-01-31 14:24:33 +0200

    Add files via upload

--- codewars.js
+++ codewars.js
@@ -200,6 +200,13 @@ function twiceAsOld(dadYearsOld, sonYearsOld) {
     return hisOld
 };
 
+function pillars (numPill, dist, width){
+  if(numPill < 2){
+    return 0;
+  } 
+    return numPill * ((dist * 100) + width) - (width * 2) - (dist * 100)
+};
+
 
 
 
