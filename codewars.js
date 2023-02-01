4d5e9eb7013b1dc8ab01c487035dbc43eaa41f5c
Arkadiy <72151941+magomedov777@users.noreply.github.com>
2023-02-01 18:03:28 +0200

    Add files via upload

--- codewars.js
+++ codewars.js
@@ -316,6 +316,14 @@ function grow(x){
       return arr.slice(0, n);
     }};
 
+    function monkeyCount(n) {
+      return Array.from({length: n},(_,index) => index + 1)
+    };
+
+    getAverage = marks => Math.floor(marks.reduce((a, b) => a + b) / marks.length);
+
+    makeUpperCase = str => str.toUpperCase();
+
     
 
 
