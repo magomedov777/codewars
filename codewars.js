9cedad8b68040b406d85c7167d7f106ee7d92809
Arkadiy <72151941+magomedov777@users.noreply.github.com>
2023-02-01 06:51:29 +0200

    Add files via upload

--- codewars.js
+++ codewars.js
@@ -80,17 +80,6 @@ function removeFirstTwo(list) {
   return shorterList;
 }
 
-const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const sourceWithoutFirstTwo = removeFirstTwo(source);
-const stats = {
-  max: 56.78,
-  standard_deviation: 4.34,
-  median: 34.54,
-  mode: 23.87,
-  min: -0.75,
-  average: 35.85
-};
-
 const half = ({ max, min }) => (max + min) / 2.0;
 
 function firstNonRepeatingLetter(s) {
@@ -256,6 +245,8 @@ function ensureQuestion(s) {
     return s + '?'
   }
 };
+
+
 
 
 
