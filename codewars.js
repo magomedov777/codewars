458c4ccb2f4d3e6498fb1eea6c73a31cd3939db8
Arkadiy <72151941+magomedov777@users.noreply.github.com>
2023-02-02 16:09:02 +0200

    Add files via upload

--- codewars.js
+++ codewars.js
@@ -368,6 +368,26 @@ function buildString(...template){
   return `I like ${template.join(', ')}!`
 };
 
+function createArray(number){
+  var newArray = [];
+  for(var counter = 1; counter <= number; counter++){
+     newArray.push(counter);
+}
+  return newArray;
+};
+
+
+function Warrior(n){
+  let name = n;  
+  this.name = function(n){
+    if(n) name = n;
+    return name;
+  }
+}
+  Warrior.prototype.toString = function(){
+    return "Hi! my name's "+this.name();
+};
+
 
 
 
