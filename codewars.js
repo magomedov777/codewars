099782ea1e270ccdcf3ae7001b333c50aa4e6973
Arkadiy <72151941+magomedov777@users.noreply.github.com>
2023-02-02 12:25:35 +0200

    Add files via upload

--- codewars.js
+++ codewars.js
@@ -356,7 +356,11 @@ function grow(x){
 
      printArray = array => array.join();
 
-     
+     function stairsIn20(s){
+  return 20 * s.reduce((a, b) => a + b.reduce((c,d) => c + d, 0) ,0) 
+};
+
+
 
 
 
