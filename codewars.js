b72c7c101dbde8d7998811d20dc026ab98d51f68
Arkadiy <alibullterrier@gmail.com>
2023-02-09 19:09:56 +0200

    if

--- codewars.js
+++ codewars.js
@@ -507,7 +507,18 @@ function correctTail(body, tail){
   return body[body.length - 1] == tail;
  };
 
- 
+
+ function howManyDalmatians(number){
+  
+  let dogs = ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIANS!!!"];
+  
+  return (number <= 10) ? dogs[0] :
+         (number <= 50) ? dogs[1] : 
+         (number == 101) ? dogs[3] :
+          dogs[2]
+  };
+
+  
 
 
      
