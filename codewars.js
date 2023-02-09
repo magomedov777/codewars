5b4647b4131460f45ab5dbdf11677848e24ea055
Arkadiy <alibullterrier@gmail.com>
2023-02-09 18:32:34 +0200

    debug

--- codewars.js
+++ codewars.js
@@ -491,6 +491,14 @@ function alternate(n, firstValue, secondValue){
   return arr;
 };
 
+function rainAmount(mm){
+  if (mm < 40) {
+       return "You need to give your plant " + (40 - mm) + "mm of water"
+  } else {
+       return "Your plant has had more than enough water for today!"
+  }
+};
+
 
 
 
