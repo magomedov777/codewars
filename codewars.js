bee8543ac0f78c99dec48208ccf0e811b9dfd307
Arkadiy <alibullterrier@gmail.com>
2023-02-10 16:39:49 +0200

    cd 8kyu

--- codewars.js
+++ codewars.js
@@ -531,6 +531,16 @@ function correctTail(body, tail){
     
   };
 
+  let humanYearsCatYearsDogYears = function(humanYears) {
+    if(humanYears == 1) return [1, 15, 15]
+    if(humanYears == 2) return [2, 24, 24]
+    return [
+      humanYears,
+      24 + (humanYears - 2) * 4,
+      24 + (humanYears - 2) * 5
+    ];
+  }
+
   
 
 
