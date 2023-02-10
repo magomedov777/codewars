2505b9763f18214e6180e0aa725c7ce182c402c9
Arkadiy <alibullterrier@gmail.com>
2023-02-10 13:30:48 +0200

    new del

--- codewars.js
+++ codewars.js
@@ -1,13 +1,4 @@
-//First kata
-function evalObject(value){
-  switch(value.operation){
-    case'+': return value.a + value.b;
-    case'-': return value.a - value.b;
-    case'/': return value.a / value.b;
-    case'*': return value.a * value.b;
-    case'%': return value.a % value.b;
-    case'^': return Math.pow(value.a, value.b);
-  }};
+
   
 function myFirstKata(a,b) {
 if (typeof(a) === 'number' && typeof(b) === 'number') {
@@ -524,7 +515,7 @@ function correctTail(body, tail){
 
 
 
-  
+
 
 
 
