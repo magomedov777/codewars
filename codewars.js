cb59358d6fb4108efdce1cf930b5528b8fb2378a
Arkadiy <alibullterrier@gmail.com>
2023-02-10 13:32:50 +0200

    del console

--- codewars.js
+++ codewars.js
@@ -29,7 +29,6 @@ function warnTheSheep(queue) {
 
   return position === 0 ? 'Pls go away and stop eating my sheep' : `Oi! Sheep number ${ position }! You are about to be eaten by a wolf!`;
 };
-console.log(queue);
 
 
 function zipWith(fn,a0,a1) {
