7193662be684ba9013ddd518c4663cb085ce6556
Arkadiy <alibullterrier@gmail.com>
2023-02-13 17:36:20 +0200

    else if

--- codewars.js
+++ codewars.js
@@ -642,7 +642,30 @@ function combat(health, damage) {
 
 sixToast = num => Math.abs(num - 6);
 
+function switchItUp(number){
+  if(number == 0){
+    return "Zero"
+  }else if(number == 1){
+   return "One"
+ }else if(number == 2){
+   return "Two"
+ }else if(number == 3){
+   return "Three"
+ }else if(number == 4){
+   return "Four"
+ }else if(number == 5){
+   return "Five"
+ }else if(number == 6){
+   return "Six"
+ }else if(number == 7){
+   return "Seven"
+ }else if(number == 8){
+   return "Eight"
+ }else if(number == 9){
+   return "Nine"
+ };
 
+ 
 
 
 
