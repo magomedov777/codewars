b69d64dce2708d474de93efc80b838c2b42ce389
Arkadiy <alibullterrier@gmail.com>
2023-02-15 06:45:43 +0200

    insert func

--- codewars.js
+++ codewars.js
@@ -814,7 +814,12 @@ function amIWilson (p){
    
    bonusTime = (salary, bonus) => bonus === true ? `£${salary * 10}` : `£${salary}`;
 
+   function reverseList(list) {
+    return list.reverse()
+  };
+
   
+
   
 
  
