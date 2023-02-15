e967acdac9ce878786e74e3bb7f14fd82c40639f
Arkadiy <alibullterrier@gmail.com>
2023-02-15 06:47:04 +0200

    refac

--- codewars.js
+++ codewars.js
@@ -1,4 +1,3 @@
-//native js training, codewars ------------------------------------------------------------
 //codewars for IT-INCUBATOR______________________________________________________________
 
 function evalObject(value){
