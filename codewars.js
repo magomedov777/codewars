157b861747a66acd9426058d1a6a34e435d35c84
Arkadiy <alibullterrier@gmail.com>
2023-02-16 08:11:05 +0200

    function

--- codewars.js
+++ codewars.js
@@ -821,16 +821,9 @@ function amIWilson (p){
 
    hello = name => !name ? 'Hello, World!' : `Hello, ${name[0].toUpperCase() + name.slice(1).toLowerCase()}!`;
 
-   function fourSeven(n){
-    const sum ={4 : 7, 7 : 4}
-    return sum[n] || 0
-   };
+  
 
-Math.round = number => number - parseInt(number) >= 0.5 ? parseInt(number) + 1 : parseInt(number);
 
-Math.ceil = number => parseInt(number) === number ? number : parseInt(number) + 1; 
-
-Math.floor = number => parseInt(number);
 
 
 
