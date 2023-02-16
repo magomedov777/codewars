64818cc1b60ac136f4ae422d31fa0e61702079b2
Arkadiy <alibullterrier@gmail.com>
2023-02-16 08:11:32 +0200

    four seven

--- codewars.js
+++ codewars.js
@@ -821,7 +821,10 @@ function amIWilson (p){
 
    hello = name => !name ? 'Hello, World!' : `Hello, ${name[0].toUpperCase() + name.slice(1).toLowerCase()}!`;
 
-  
+   function fourSeven(n){
+    const sum ={4 : 7, 7 : 4}
+    return sum[n] || 0
+   };
 
 
 
