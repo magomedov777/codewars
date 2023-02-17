cb7654246e74a323b5fbfce0e1f325cbb184d39c
Arkadiy <alibullterrier@gmail.com>
2023-02-17 05:53:31 +0200

    sp tasks

--- codewars.js
+++ codewars.js
@@ -502,24 +502,20 @@ function correctTail(body, tail){
 
 
  function howManyDalmatians(number){
-  
   let dogs = ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIANS!!!"];
-  
   return (number <= 10) ? dogs[0] :
          (number <= 50) ? dogs[1] : 
          (number == 101) ? dogs[3] :
           dogs[2]
   };
 
-  weatherInfo = temp => `${c = (temp - 32) * 5 / 9} is ${c > 0 ? 'above ' : ''}freezing temperature`;
+weatherInfo = temp => `${c = (temp - 32) * 5 / 9} is ${c > 0 ? 'above ' : ''}freezing temperature`;
 
- 
-  const areaOrPerimeter = function(l , w) {
+ const areaOrPerimeter = function(l , w) {
     if(l == w) {
       return l * w
     }
       return (l + w)*2
-    
   };
 
 let humanYearsCatYearsDogYears = function(humanYears) {
