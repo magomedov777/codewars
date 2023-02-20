00d88fd94fb778c4f45c169d18dcc339dfb5d5f8
Arkadiy <alibullterrier@gmail.com>
2023-02-20 06:03:55 +0200

    reverse func

--- codewars.js
+++ codewars.js
@@ -324,7 +324,12 @@ const removeExclamationMarks = s => s.replace(/\!/g, '');
 
 billboard = (name, price = 30) => [...new Array(name.length)].fill(price).reduce((a,b) => a + b);
 
-
+function countBy(x, n) {
+      let z = [];
+      for(let i = 1; i <= n; i++)
+       z.push(i * x)
+        return z;
+    };
 
 removeEveryOther = arr => arr.filter((_, index) => index % 2 == 0);
 
@@ -952,7 +957,7 @@ function between(a, b) {
     return listOfNumbers.concat(['new'])
  };
 
-
+ 
 
  
 
