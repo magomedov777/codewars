3e77cc4e8d5a517f8f397670640f915ede83e788
Arkadiy <alibullterrier@gmail.com>
2023-02-20 18:32:18 +0200

    Math.floor method=====>

--- codewars.js
+++ codewars.js
@@ -976,6 +976,13 @@ getAge = inputString => parseInt(inputString);
 
 findDifference = (a,b) => Math.abs(a.reduce((x,y) => x * y) - b.reduce((x,y) => x * y));
 
+function cockroachSpeed(s) {
+ const speed = s * 1000 * 100 / (60 * 60)
+ return Math.floor(speed)
+};
+
+cockroach = s => Math.floor(s * 1000 * 100 / (60 * 60));
+
 
  
 
