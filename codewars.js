bd43f83e6b4c55435077d361a3d8c15cb300b4e3
Arkadiy <alibullterrier@gmail.com>
2023-02-23 09:03:22 +0200

    grow function, arrow syntax

--- codewars.js
+++ codewars.js
@@ -265,6 +265,8 @@ function grow(x){
   return x.reduce((a,b) => a * b)
  };
 
+grow = x => x.reduce((a,b) => a * b);
+
  function howMuchILoveYou(nbPetals) {
   return ["I love you", "a little", "a lot", "passionately", "madly", "not at all"][(nbPetals - 1) %6]
   };
