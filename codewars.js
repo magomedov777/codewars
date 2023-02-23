c1a4b01df9e362bf0ef80805d1f264bc6154e16f
Arkadiy <alibullterrier@gmail.com>
2023-02-23 09:01:44 +0200

    square sum func

--- codewars.js
+++ codewars.js
@@ -403,6 +403,8 @@ function squareSum(numbers){
   return numbers.map(x => x * x).reduce((a,b) => a + b, 0)
 };
 
+squareSum = numbers => numbers.map(x => x * x).reduce((a,b) => a + b, 0);
+
 function smash (words) {
   return words.join(' ')
 };
