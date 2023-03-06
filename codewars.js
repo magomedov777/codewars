e37a4fb605fc1570bab00106ab800ae344202a87
Arkadiy <alibullterrier@gmail.com>
2023-03-06 06:38:31 +0200

    rm

--- codewars.js
+++ codewars.js
@@ -85,9 +85,6 @@ function half({max,min}){
   return (max + min) / 2.0;
 }
 
-function firstNonRepeatingLetter(s) {
-  return s[s.toLowerCase().split('').findIndex(letter => s.toLowerCase().split('').filter(l => l === letter).length === 1 )] || '';
-}
 
 function balancedNum(number) {
   const stringNum = String(number);
