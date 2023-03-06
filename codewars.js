edbeceea8541b4847afe91e3f94e643a7a69455f
Arkadiy <alibullterrier@gmail.com>
2023-03-06 06:35:33 +0200

    delete func

--- codewars.js
+++ codewars.js
@@ -1,5 +1,4 @@
 //Codewars for IT-INCUBATOR 
-//Road to 1 kyu ==========================================>>>>>>>
 //start 22 January, 2023
 
 function evalObject(value){                     //new switch case
