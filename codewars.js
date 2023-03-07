12966bed8d5614bdba5af13d95185798135f80a4
Arkadiy <alibullterrier@gmail.com>
2023-03-07 08:52:48 +0200

    remove

--- codewars.js
+++ codewars.js
@@ -1,15 +1,7 @@
 //Codewars for IT-INCUBATOR 
 //start 22 January, 2023
 
-function evalObject(value){                   
-  switch(value.operation){
-    case'+': return value.a + value.b;
-    case'-': return value.a - value.b;
-    case'/': return value.a / value.b;
-    case'*': return value.a * value.b;
-    case'%': return value.a % value.b;
-    case'^': return Math.pow(value.a, value.b);
-}};
+
 
 function myFirstKata(a,b) {
   if (typeof(a) === 'number' && typeof(b) === 'number') {
