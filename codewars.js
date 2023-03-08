2af4e5317e28a75efee5c13e676038eb638e3efa
Arkadiy <alibullterrier@gmail.com>
2023-03-08 10:14:34 +0200

    new func

--- codewars.js
+++ codewars.js
@@ -703,6 +703,13 @@ solution = str => [...str].reverse().join("");
 
 simpleMultiplication = number => number % 2===0 ? number * 8 : number * 9;
 
+function simpleMultiplication(number){
+  if(number % 2 === 0){
+    return number * 8
+  }
+  return number * 9
+};
+
 function position (letters){
 const alphbt = ' abcdefghijklmnopqrstuvwxyz';
   return 'Position of alphabet: ' + [...alphbt].indexOf(letters)
