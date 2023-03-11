395cae8f93638e4b2c06095e6a14f941a8449e5b
Arkadiy <alibullterrier@gmail.com>
2023-03-11 09:18:31 +0200

    sum of differences sum

--- codewars.js
+++ codewars.js
@@ -438,6 +438,8 @@ function sumOfDifferences(arr) {
   }
 };
 
+sumOfDifferences = arr => arr.length > 1 ? Math.max(...arr) - Math.min(...arr) : 0;
+
 function twoSort(s) {
   return s.sort()[0].split('').join("***")
 };
