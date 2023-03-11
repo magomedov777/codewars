63a939534b1fbf54d8ec16858dc17e236c27a9cd
Arkadiy <alibullterrier@gmail.com>
2023-03-11 09:12:34 +0200

    a,b

--- codewars.js
+++ codewars.js
@@ -528,7 +528,7 @@ function capitalizeWord (word) {
   return word[0].toUpperCase() + word.slice(1);
 };
 
-sameCase = (a,b) => /[a-z]/i.test(a) && /[a-z]/i.test(b) ? Number(/[a-z]/.test(a) == /[a-z]/.test(b)) : -1;
+sameCase = (a, b) => /[a-z]/i.test(a) && /[a-z]/i.test(b) ? Number(/[a-z]/.test(a) == /[a-z]/.test(b)) : -1;
 
 function nameMe(first, last) {
   this.firstName = first;
