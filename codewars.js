3092b663e787c62083c1a7dcdf332f780de33d7f
Arkadiy <alibullterrier@gmail.com>
2023-03-12 10:07:09 +0200

    rm

--- codewars.js
+++ codewars.js
@@ -699,7 +699,6 @@ function distanceBetweenPoints(a, b) {
     return Math.hypot(a.x - b.x, a.y - b.y);
 };
 
-const distanceBetweenPoints = (a, b) => Math.hypot(a.x - b.x, a.y - b.y);
 
 function mystery() {
   const results = {sanity: 'Hello'};
