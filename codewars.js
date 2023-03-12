54f5872cf54c4828ca4193c2ea4fce06315a42f0
Arkadiy <alibullterrier@gmail.com>
2023-03-12 10:05:21 +0200

    add new func

--- codewars.js
+++ codewars.js
@@ -887,6 +887,13 @@ abbrevName = name => name.toUpperCase().split(' ').map(a => a[0]).join(`.`);
 
 oddCount = n => n % 2 === 0 ? n / 2 : Math.floor(n / 2);
 
+function oddCount(n){
+  if(n % 2 === 0){
+    return n / 2 
+  }
+  Math.floor(n / 2)
+};
+
 removeChar = str => str.slice(1, -1);
 
 function removeChar(str){
