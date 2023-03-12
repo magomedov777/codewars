55e81a3040e96e3678b883cb52cda0011eda1f58
Arkadiy <alibullterrier@gmail.com>
2023-03-12 10:02:32 +0200

    new ref

--- codewars.js
+++ codewars.js
@@ -912,6 +912,10 @@ testEven = n => n % 2 === 0 ? true : false;
 
 noSpace = x => x.replace(/ /g, '');
 
+function noSpace(x){
+  return x.replace(/ /g, '');
+};
+
 function mouthSize(animal) {
   if(animal === 'alligator' || animal === 'ALLIGATOR'){
     return 'small'
