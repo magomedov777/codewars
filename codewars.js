062f50a7387775a857d7bc712b099abba4787616
Arkadiy <alibullterrier@gmail.com>
2023-03-13 06:26:09 +0200

    new add

--- codewars.js
+++ codewars.js
@@ -401,7 +401,9 @@ function printArray(array){
   return array.join();
 };
 
-
+function stairsIn20(s){
+  return 20 * s.reduce((a, b) => a + b.reduce((c,d) => c + d, 0) ,0) 
+};
 
 stairsIn20 = s => 20 * s.reduce((a, b) => a + b.reduce((c,d) => c + d, 0) ,0);
 
