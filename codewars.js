68f28d72af1dcbab043b62ab0b5138f3bba23402
Arkadiy <alibullterrier@gmail.com>
2023-03-15 09:39:48 +0200

    add new

--- codewars.js
+++ codewars.js
@@ -187,7 +187,9 @@ function multipleOfIndex(array){
   return array.filter((element, index) => element % index == 0)
 };
 
-
+function check(a,x){
+  return a.includes(x)
+};
 
 var joinStrings = (string1, string2) => `${string1} ${string2}`;
 
