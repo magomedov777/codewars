0b8f8f6d3680d153430fdc8c5e07441fcce69dd7
Arkadiy <alibullterrier@gmail.com>
2023-03-15 09:37:02 +0200

    rm

--- codewars.js
+++ codewars.js
@@ -100,24 +100,7 @@ function balancedNum(number) {
         : 'Not Balanced';
 };
 
-function whatday(num) 
-  if(num < 1 || num > 7){
-    return "Wrong, please enter a number between 1 and 7"
-  }else if(num === 1){
-   return "Sunday"
- }else if(num === 2){
-   return "Monday"
- }else if(num === 3){
-   return "Tuesday"
- }else if(num === 4){
-   return "Wednesday"
- }else if(num === 5){
-   return "Thursday"
- }else if(num === 6){
-   return "Friday"
- }else if(num === 7){
-   return "Saturday"
- };
+
 
  function century(year) {
   return Math.ceil(year / 100)
