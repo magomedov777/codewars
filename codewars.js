61c97539bfde910c659bee63e8f170eb1b8a5722
Arkadiy <alibullterrier@gmail.com>
2023-03-15 09:40:28 +0200

    remove

--- codewars.js
+++ codewars.js
@@ -224,16 +224,7 @@ function finalGrade (exam, projects) {
   }
 };
 
-const quarterOf = (month) => {
-  if(month >= 1 && month <= 3){
-    return 1;
-  }else if(month >= 3 && month <= 6){
-    return 2;
-  }else if(month >= 7 && month <= 9){
-    return 3;
-  }else(month >= 10 && month <= 12)
-    return 4;
-};
+
 
 function typeValidation(variable, type) {
   return typeof variable === type; 
