f4077b0782eb99cf26a151f1c560ea1e9af86f7f
Arkadiy <alibullterrier@gmail.com>
2023-03-16 07:20:57 +0200

    arrow func, new task

--- codewars.js
+++ codewars.js
@@ -11,12 +11,14 @@ function evalObject(value){
     case'^': return Math.pow(value.a, value.b);
 }};
 
-function myFirstKata(a,b) {
+function myFirstKata(a, b) {
   if (typeof(a) === 'number' && typeof(b) === 'number') {
     return (a % b) + (b % a);
   } else {
 return false;
 }};
+
+myFirstKata = (a, b) => typeof(a) === 'number' && typeof(b) === 'number' ? (a % b) + (b % a) : false;
 
 function warnTheSheep(queue) {
   const position = queue.reverse().indexOf('wolf');
