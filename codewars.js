ad7a043b7ddcc4711342e3d48d74d0a8f70770b9
Arkadiy <alibullterrier@gmail.com>
2023-03-17 06:36:31 +0200

    new

--- codewars.js
+++ codewars.js
@@ -614,7 +614,7 @@ function rainAmount(mm){
   } else {
        return "Your plant has had more than enough water for today!"
   }};
-
+  
 rainAmount = mm => mm < 40 ? "You need to give your plant " + (40 - mm) + "mm of water" :
 "Your plant has had more than enough water for today!";
 
