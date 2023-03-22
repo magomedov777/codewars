402e7c4351622cf1e69ac916ea7c0197d52b2a77
Arkadiy <alibullterrier@gmail.com>
2023-03-22 06:26:22 +0200

    remove

--- codewars.js
+++ codewars.js
@@ -1565,9 +1565,9 @@ function nameShuffler (str){
 
 nameShuffler = str => str.split(' ').reverse().join(' ');
 
-sumArray = array => array ? array.sort((a, b) => a - b).slice(1,-1).reduce((x, y) => x + y, 0) : 0;
 
-correct = string => string.replace(/5/g, 'S').replace(/0/g, 'O').replace(/1/g, 'I')
+
+
 
 
   
