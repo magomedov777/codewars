5e17fb5558cfb068747ab442aaa991447fd2395d
Arkadiy <alibullterrier@gmail.com>
2023-03-25 08:23:20 +0200

    add half func

--- codewars.js
+++ codewars.js
@@ -87,7 +87,7 @@ function half({max, min}){
   return (max + min) / 2.0;
 };
 
-
+half = (max, min) => (max + min) / 2.0;
 
 function firstNonRepeatingLetter(s) {
   return s[s.toLowerCase().split('').findIndex(letter => s.toLowerCase().split('').filter(l => l === letter).length === 1 )] || '';
