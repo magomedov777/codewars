3c697d13a21f2b261f365ba4913b1d6c799ce367
Arkadiy <alibullterrier@gmail.com>
2023-03-29 06:41:34 +0300

    rm count

--- codewars.js
+++ codewars.js
@@ -515,9 +515,7 @@ sum = numbers => numbers.reduce((a,b) => a + b, 0);
 
 countSheeps = arrayOfSheep => arrayOfSheep.filter(Boolean).length;
 
-function countSheeps (arrayOfSheep){
-  return arrayOfSheep.filter(Boolean).length;
-};
+
 
 function digitize (n){
   return String(n).split('').reverse().map(Number);
