3ffdbf3dbab7bb38e19869e2c6df899fbf619c25
Arkadiy <alibullterrier@gmail.com>
2023-04-04 00:30:32 +0300

    return new

--- codewars.js
+++ codewars.js
@@ -954,7 +954,12 @@ function abbrevName(name){
 
 oddCount = n => n % 2 === 0 ? n / 2 : Math.floor(n / 2);
 
-
+function oddCount(n){
+  if(n % 2 === 0){
+    return n / 2 
+  }
+  Math.floor(n / 2)
+};
 
 removeChar = str => str.slice(1, -1);
 
