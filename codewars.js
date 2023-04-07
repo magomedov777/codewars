6a117574676bac0c830f96754ec4ebcf1f08611a
Arkadiy <alibullterrier@gmail.com>
2023-04-07 06:57:19 +0300

    arr sort

--- codewars.js
+++ codewars.js
@@ -1148,6 +1148,7 @@ min = list => Math.min(...list);
   
 max = list => Math.max(...list);
 
+const flip = (d, a) => d === "R" ? a.sort((a,b) => a - b) : a.sort((a, b) => b - a);
 
 function sakuraFall(v) {
   if(v > 0){
