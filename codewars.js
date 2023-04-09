89d55330311b3f42d3cdd5dec6a5f98e71e233d0
Arkadiy <alibullterrier@gmail.com>
2023-04-09 07:40:56 +0300

    [arr]

--- codewars.js
+++ codewars.js
@@ -1277,13 +1277,7 @@ function padIt(str, n){
   return str;
 };
 
-function between(a, b) {
-  const array = []
-    for(i = a; i <= b; i++){
-      array.push(i)
-    }
-    return array
-  };
+
 
   function addExtra( listOfNumbers ){
     return listOfNumbers.concat(['new'])
