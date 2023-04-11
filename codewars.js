3eb86665543f0cbaa86391a979ba393e61c29daf
Arkadiy <alibullterrier@gmail.com>
2023-04-11 09:06:05 +0300

    new task

--- codewars.js
+++ codewars.js
@@ -1313,7 +1313,6 @@ function getAge(inputString){
   return parseInt(inputString);
 };
 
-getAge = inputString => parseInt(inputString);
 
 findDifference = (a,b) => Math.abs(a.reduce((x,y) => x * y) - b.reduce((x,y) => x * y));
 
