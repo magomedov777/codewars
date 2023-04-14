d0b99d4f7e59e0d3dfda5435ffdf74fb0878a3ba
Arkadiy <alibullterrier@gmail.com>
2023-04-15 00:02:35 +0300

    new arrow

--- codewars.js
+++ codewars.js
@@ -317,6 +317,7 @@ grow = x => x.reduce((a,b) => a * b);
   return ["I love you", "a little", "a lot", "passionately", "madly", "not at all"][(nbPetals - 1) %6]
   };
 
+howMuchILoveYou = nbPetals => ["I love you", "a little", "a lot", "passionately", "madly", "not at all"][(nbPetals - 1) %6];
 
 function well(x){
     let good = 0;
