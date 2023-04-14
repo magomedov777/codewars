97780be9c430002a67afcc21727c777415b3b14e
Arkadiy <alibullterrier@gmail.com>
2023-04-15 00:03:43 +0300

    ref

--- codewars.js
+++ codewars.js
@@ -369,7 +369,7 @@ function first (arr, n = 1){
 
 function monkeyCount(n) {
       return Array.from({length: n},(_,index) => index + 1)
-    };
+}
 
 monkeyCount = n => Array.from({length: n}, (_,index) => index + 1);
 
