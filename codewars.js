697d4f20c0aae26ad56123ed969a9417ff8304b1
Arkadiy <alibullterrier@gmail.com>
2023-04-26 06:33:52 +0300

    find difference func

--- codewars.js
+++ codewars.js
@@ -1315,6 +1315,7 @@ function getAge(inputString){
 
 getAge = inputString => parseInt(inputString);
 
+findDifference = (a,b) => Math.abs(a.reduce((x,y) => x * y) - b.reduce((x,y) => x * y));
 
 function findDifference(a, b) {
   return Math.abs(a.reduce((x,y) => x * y) - b.reduce((x,y) => x * y));
