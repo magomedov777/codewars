084b4541bb66b4e5a555eac7cdc120e25b1983b5
Arkadiy <alibullterrier@gmail.com>
2023-04-29 05:25:57 +0300

    remove for

--- codewars.js
+++ codewars.js
@@ -1586,10 +1586,7 @@ function odd(values){
 
 const newOdds = (values) => values.filter(el => el % 2);
 
-const arr = [1, 2, 3, 4, 5, 6, 7]
-for (let i = 0; i < arr.length; i++) {
-  const element = array[i];
-}
+
 
 
 
