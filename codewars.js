f7f16ee6869879311b31d7c2a3a65a37384f6380
Arkadiy <alibullterrier@gmail.com>
2023-05-05 06:37:10 +0300

    new

--- codewars.js
+++ codewars.js
@@ -1607,13 +1607,6 @@ function shortenToDate(longDate) {
 
 shortenToDate = longDate => longDate.split(',')[0];
 
-function firstNonConsecutive (arr) {
-  for(let i = 1; i < arr.length; i++)
-    if(arr[i] - arr[i - 1] !== 1){
-      return arr[i]
-    }
-  return null
-};
 
 
 
