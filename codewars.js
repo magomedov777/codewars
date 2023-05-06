b3b84d6bf2f81f68ecd78c999c9e40759013faa4
Arkadiy <alibullterrier@gmail.com>
2023-05-06 07:26:49 +0300

    new correct

--- codewars.js
+++ codewars.js
@@ -1573,6 +1573,7 @@ nameShuffler = str => str.split(' ').reverse().join(' ');
 
 sumArray = array => array ? array.sort((a, b) => a - b).slice(1,-1).reduce((x, y) => x + y, 0) : 0;
 
+correct = string => string.replace(/5/g, 'S').replace(/0/g, 'O').replace(/1/g, 'I');
 
 
 const newOdds = (values) => values.filter(el => el % 2);
