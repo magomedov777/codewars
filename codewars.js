7b5207db8ac81258ff4c8a1f8a963b67092733de
Arkadiy <alibullterrier@gmail.com>
2023-05-08 06:51:06 +0300

    experemental solution

--- codewars.js
+++ codewars.js
@@ -367,9 +367,7 @@ function first (arr, n = 1){
       return arr.slice(0, n);
     }};
 
-function monkeyCount(n) {
-      return Array.from({length: n},(_,index) => index + 1)
-}
+
 
 monkeyCount = n => Array.from({length: n}, (_,index) => index + 1);
 
