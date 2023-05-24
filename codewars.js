7f152793f20b4a5535faee6ea26f0c6726064259
Arkadiy <alibullterrier@gmail.com>
2023-05-24 05:19:07 +0300

    changed

--- codewars.js
+++ codewars.js
@@ -1605,15 +1605,9 @@ function shortenToDate(longDate) {
   return longDate.split(',')[0]
 };
 
-shortenToDate = longDate => longDate.split(',')[0];
 
-function firstNonConsecutive (arr) {
-  for(let i = 1; i < arr.length; i++)
-    if(arr[i] - arr[i - 1] !== 1){
-      return arr[i]
-    }
-  return null
-};
+
+
 
 
 
