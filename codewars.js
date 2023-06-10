7bd617de9b4db37a168d86219c7c9029ad4d6715
Arkadiy <alibullterrier@gmail.com>
2023-06-11 00:02:08 +0300

    new

--- codewars.js
+++ codewars.js
@@ -1605,25 +1605,9 @@ function shortenToDate(longDate) {
   return longDate.split(',')[0]
 };
 
-shortenToDate = longDate => longDate.split(',')[0];
 
-function firstNonConsecutive (arr) {
-  for(let i = 1; i < arr.length; i++)
-    if(arr[i] - arr[i - 1] !== 1){
-      return arr[i]
-    }
-  return null
-};
 
-class SmallestIntegerFinder {
-  findSmallestInt(args) {
-    return Math.min(...args)
-  }
-};
 
-const findSmallestInt = (args) => {
-  args.sort((a, b) => a - b)[0]
-};
 
 
 
