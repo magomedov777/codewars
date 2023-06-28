79244ea400cb1d37882919f978e4d829aa881863
Arkadiy <alibullterrier@gmail.com>
2023-06-28 06:01:01 +0300

    concat arr

--- codewars.js
+++ codewars.js
@@ -1639,6 +1639,10 @@ export function stringToArray(s) {
 	return s.split(' ');
 };
 
+export const arrayPlusArray = (arr1, arr2) => {
+  return [...arr1, ...arr2].reduce((acc, cur) => acc + cur)
+}
+
 
 
 
