d75a511d24c06e072dca46b3b457dc7b670ae3e4
Arkadiy <alibullterrier@gmail.com>
2023-07-05 06:05:27 +0300

    exp

--- codewars.js
+++ codewars.js
@@ -279,6 +279,7 @@ function ensureQuestion(s) {
 
 ensureQuestion = s => s.endsWith('?') ? s : s = '?';
 
+const largest = (n, array) => array.sort((a,b) => a - b).slice(array.length - n);
 
 function largest(n, array){
   return array.sort((a,b) => a - b).slice(array.length - n);
