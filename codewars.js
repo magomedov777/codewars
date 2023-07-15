019c507106ab7a5b15bddc8b493c28fadf4d6c70
Arkadiy <alibullterrier@gmail.com>
2023-07-15 06:13:19 +0300

    fast solution

--- codewars.js
+++ codewars.js
@@ -1401,7 +1401,6 @@ function alphabet(ns) {
   return srt[7] / srt[srt[0] * srt[1] === srt[2] ? 3 : 2]
 };
 
-lastSurvivor = (letters, coords) => coords.reduce((acc,curr) => acc.slice(0, curr) + acc.slice(curr + 1), letters);
 
 function Counter() {
   this.value = 0;
