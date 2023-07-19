e359f4205547285dfc7c82de6aa098487af7e00e
Arkadiy <alibullterrier@gmail.com>
2023-07-19 06:12:59 +0300

    return func

--- codewars.js
+++ codewars.js
@@ -416,6 +416,7 @@ function removeEveryOther(arr){
   return arr.filter((_, index) => index % 2 == 0);
 };
 
+differenceInAges = ages => [Math.min(...ages), Math.max(...ages), Math.max(...ages) - Math.min(...ages)];
 
 function all( arr, fun ){
       return arr.every(fun)
