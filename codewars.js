51894e71a7bccf9d28c7a2b3514aa7d6b5d9212c
Arkadiy <alibullterrier@gmail.com>
2023-07-21 05:18:14 +0300

    [...arr]

--- codewars.js
+++ codewars.js
@@ -762,6 +762,7 @@ function solution(str){
   return str = [...str].reverse().join("")
 };
 
+solution = str => [...str].reverse().join("");
 
 simpleMultiplication = number => number % 2===0 ? number * 8 : number * 9;
 
