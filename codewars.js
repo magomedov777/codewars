c78b547cdc99dd767a154e0921984638bae294e7
Arkadiy <alibullterrier@gmail.com>
2023-07-23 09:36:54 +0300

    str work

--- codewars.js
+++ codewars.js
@@ -612,6 +612,7 @@ function rainAmount(mm){
   if (mm < 40) {
        return "You need to give your plant " + (40 - mm) + "mm of water"
   } else {
+       return "Your plant has had more than enough water for today!"
   }};
   
 rainAmount = mm => mm < 40 ? "You need to give your plant " + (40 - mm) + "mm of water" :
