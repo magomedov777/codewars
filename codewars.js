b93c921944a0d68d5673fc9e48cddbac064901d3
Arkadiy <alibullterrier@gmail.com>
2023-07-29 00:01:34 +0300

    update func

--- codewars.js
+++ codewars.js
@@ -1118,9 +1118,7 @@ remove = string => string.replace(/!$/, "");
 
 add = (a, b) => a == b ? true : false;
 
-function add(a, b){
-  return a == b ? true : false;
-};
+
 
 function add(a, b){
   if(a == b){
